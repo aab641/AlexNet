a557b406da7c7295b159fdb1d99844a4b9b735f1
Alexander Bedrossian <aab641@nyu.edu>
2019-02-05 14:32:40 -0500

    Initial Commit

---
+++ AlexNet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlexNet
+//
+//  Created by Alexander Bedrossian on 2/5/19.
+//  Copyright © 2019 Bedrossian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
